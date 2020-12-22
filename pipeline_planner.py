593654e52aeb549e947bf223b1ffce4e43ba12ad
kieemi <kielaremil@gmail.com>
2020-12-22 20:36:39 +0100

    Table populates for raptor nests

--- pipeline_planner.py
+++ pipeline_planner.py
@@ -21,9 +21,12 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
-from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt
+from qgis.PyQt.QtGui import QIcon 
+from qgis.PyQt.QtWidgets import QAction, QApplication, QMessageBox, QTableWidgetItem
+
+from qgis.gui import QgsMapToolEmitPoint, QgsRubberBand
+from qgis.core import QgsProject
 
 # Initialize Qt resources from file resources.py
 from .resources import *
@@ -45,9 +48,20 @@ class PipelinePlanner:
         """
         # Save reference to the QGIS interface
         self.iface = iface
-        # initialize plugin directory
+        #create reference to map.Canvas
+        self.canvas = self.iface.mapCanvas()
+        
+        # initialize plugins directory      
         self.plugin_dir = os.path.dirname(__file__)
         # initialize locale
+        #Initialize QgsMapToolEmitPoint class, and pass map canvas where we will emit the point
+        self.addPipelinePoint = QgsMapToolEmitPoint(self.canvas)
+        #create instance of rubberband class
+        self.rbPipeline = QgsRubberBand(self.canvas)
+        self.rbPipeline.setColor(Qt.red)
+        self.rbPipeline.setWidth(4)
+        
+        
         locale = QSettings().value('locale/userLocale')[0:2]
         locale_path = os.path.join(
             self.plugin_dir,
@@ -62,10 +76,18 @@ class PipelinePlanner:
         # Declare instance attributes
         self.actions = []
         self.menu = self.tr(u'&Pipeline Planner')
+        
+        #create instance of results dialog
+        self.dlg = PipelinePlannerDialog()
 
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
         self.first_start = None
+        
+        self.dlg = PipelinePlannerDialog()
+        self.dlg.tblImpacts.setColumnWidth(1, 50)
+        self.dlg.tblImpacts.setColumnWidth(2, 225)
+        self.dlg.tblImpacts.setColumnWidth(3, 75)
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -169,7 +191,9 @@ class PipelinePlanner:
 
         # will be set False in run()
         self.first_start = True
-
+        
+        #connect to mapCanvas click event
+        self.addPipelinePoint.canvasClicked.connect(self.evaluatePipeline)
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
@@ -182,19 +206,39 @@ class PipelinePlanner:
 
     def run(self):
         """Run method that performs all the real work"""
-
-        # Create the dialog with elements (after translation) and keep reference
-        # Only create GUI ONCE in callback, so that it will only load when the plugin is started
-        if self.first_start == True:
-            self.first_start = False
-            self.dlg = PipelinePlannerDialog()
-
-        # show the dialog
-        self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+        # set the canvas maptool
+        self.canvas.setMapTool(self.addPipelinePoint)
+        
+       #method to control drawning proces 
+    def evaluatePipeline(self, point, button):
+        if button == Qt.LeftButton:
+            self.rbPipeline.addPoint(point)
+            self.rbPipeline.show()
+        elif button == Qt.RightButton:
+            pipeline = self.rbPipeline.asGeometry()
+#             QMessageBox.information(None, "Pipeline", pipeline.asWkt())
+            self.dlg.tblImpacts.setRowCount(0)
+            #create reference to raptor_buffer layer
+            
+            lyrRaptor = QgsProject.instance().mapLayersByName("Raptor Buffer")[0]
+            
+            raptors = lyrRaptor.getFeatures(pipeline.boundingBox())
+            for raptor in raptors:
+                valConstraint = raptor.attribute("recentspec")
+                valID = raptor.attribute("Nest_ID")
+                valStatus = raptor.attribute("recentstat")
+                valDistance = pipeline.distance(raptor.geometry().centroid())
+                row = self.dlg.tblImpacts.rowCount()
+                if raptor.geometry().intersects(pipeline):
+                    self.dlg.tblImpacts.insertRow(row)
+                    self.dlg.tblImpacts.setItem(row, 0,QTableWidgetItem(valConstraint))
+                    self.dlg.tblImpacts.setItem(row, 1,QTableWidgetItem(str(valID)))
+                    self.dlg.tblImpacts.setItem(row, 2,QTableWidgetItem(valStatus))
+                    self.dlg.tblImpacts.setItem(row, 3,QTableWidgetItem("{:4.5f}".format(valDistance)))
+                
+            self.dlg.show()        
+                
+                
+            
+            self.rbPipeline.reset()
+    
